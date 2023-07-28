c1bc01c587ccf8368644b0ab4f0adf5572d228be
nisidazza <nisida.azzalini@gmail.com>
2023-07-28 16:37:40 +1200

    tracks-on-tracks-on-tracks solution

---
+++ csharp/tracks-on-tracks-on-tracks/TracksOnTracksOnTracks.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+public static class Languages
+{
+    public static List<string> NewList()
+    {
+        return new List<string>();
+    }
+
+    public static List<string> GetExistingLanguages()
+    {
+        return new List<string> { "C#", "Clojure", "Elm" };
+    }
+
+    public static List<string> AddLanguage(List<string> languages, string language)
+    {
+        languages.Add(language);
+        return languages;
+    }
+
+    public static int CountLanguages(List<string> languages)
+    {
+        return languages.Count;
+    }
+
+    public static bool HasLanguage(List<string> languages, string language)
+    {
+        return languages.Contains(language);
+    }
+
+    public static List<string> ReverseList(List<string> languages)
+    {
+        languages.Reverse();
+        return languages;
+    }
+
+    public static bool IsExciting(List<string> languages)
+    {
+        int cSharpIndex = languages.IndexOf("C#");
+        return cSharpIndex == 0 || cSharpIndex == 1 && (languages.Count <= 3);
+    }
+
+    public static List<string> RemoveLanguage(List<string> languages, string language)
+    {
+        languages.Remove(language);
+        return languages;
+    }
+
+    public static bool IsUnique(List<string> languages)
+    {
+        return languages.Distinct().Count() == languages.Count;
+    }
+}
